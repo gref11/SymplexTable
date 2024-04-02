1fe567551d44f25c698d5cdf9ce207081d6e2383
grefalb <grefalb11@gmail.com>
2024-04-02 10:14:58 +0300

    updated Fract class

--- fraction.py
+++ fraction.py
@@ -1,3 +1,5 @@
+from numpy import sign
+
 def nod(a, b):
     if type(a) != type(b) != type(1) or a < 1 or b < 1:
         print("Incorect value of numbers")
@@ -14,7 +16,7 @@ class Fract:
     __num = 1
     __den = 1
 
-    def __init__(self, num, den=1):
+    def __init__(self, num, den):
         if type(num) != type(1) or int(num) != num:
             print(f"Incorrect numerator value: {num}")
             return
@@ -41,21 +43,68 @@ class Fract:
         res_den = fract1.__den * fract2.__den
         nod_fract = nod(abs(res_num), res_den)
         return Fract(int(res_num / nod_fract), int(res_den / nod_fract))
+    
+    def __iadd__(self, other):
+        res = self + other
+        self.__num = res.__num
+        self.__den = res.__den
+        return self
 
     def __mul__(fract1, fract2):
         res_num = fract1.__num * fract2.__num
         res_den = fract1.__den * fract2.__den
         nod_fract = nod(abs(res_num), res_den)
         return Fract(int(res_num / nod_fract), int(res_den / nod_fract))
+    
+    def __imul__(self, other):
+        res = self * other
+        self.__num = res.__num
+        self.__den = res.__den
+        return self
+    
+    def __sub__(fract1, fract2):
+        res_num = fract1.__num * fract2.__den - fract2.__num * fract1.__den
+        res_den = fract1.__den * fract2.__den
+        nod_fract = nod(abs(res_num), res_den)
+        return Fract(int(res_num / nod_fract), int(res_den / nod_fract))
+    
+    def __isub__(self, other):
+        res = self - other
+        self.__num = res.__num
+        self.__den = res.__den
+        return self
+    
+    def __truediv__(fract1, fract2):
+        res_num = fract1.__num * fract2.__den * sign(fract2.__num)
+        res_den = fract1.__den * abs(fract2.__num)
+        nod_fract = nod(abs(res_num), res_den)
+        return Fract(int(res_num / nod_fract), int(res_den / nod_fract))
+    
+    def __itruediv__(self, other):
+        res = self / other
+        self.__num = res.__num
+        self.__den = res.__den
+        return self
 
 
 if __name__ == "__main__":
-    fr1 = Fract(1, 2)
-    fr2 = Fract(-1, 2)
-    fr3 = Fract(6, 12)
-    fr4 = Fract(0, 2)
+    # fr1 = Fract(1, 2)
+    # fr2 = Fract(-1, 2)
+    # fr3 = Fract(6, 12)
+    # fr4 = Fract(0, 2)
     # print(fr1, fr2, fr3, fr4)
     # print(fr1 + fr4)
-    print(Fract(2, 3) * Fract(3, 4))
-    print(Fract(1, 3) + Fract(1, 6))
-    print(type(Fract(1, 2)))
+    # print(Fract(2, 3) * Fract(3, 4))
+    # print(Fract(1, 3) + Fract(1, 6))
+    # print(Fract(1, 3) - Fract(1, 6))
+    # print(Fract(1, 3) / Fract(1, 6))
+    fr = Fract(1, 2)
+    print(fr)
+    fr += Fract(-1, 6)
+    print(fr)
+    fr -= Fract(-1, 6)
+    print(fr)
+    fr *= Fract(-1, 6)
+    print(fr)
+    fr /= Fract(-1, 6)
+    print(fr)
